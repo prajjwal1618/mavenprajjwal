72db377fb90c8764e6c299938403dca662f7ca99
Prajjwal Chaubey <Prajjwal.anand@gmail.com>
2022-09-21 15:51:09 +0530

    first commit of maven integration with jenkins

---
+++ src/main/java/in/PrajjwalChaubey/App.java
@@ -0,0 +1,20 @@
+package in.PrajjwalChaubey;
+import java.util.*;
+
+public class App 
+{
+    /**
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter 2 numbers : ");
+        
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println((num1+num2)+" is apple od!!");
+    }
+}
